c818f4cca31fdeedb57d607a0eb0a8d359a2d4de
ken-alt <kenec@mac.com>
2026-05-11 10:13:00 -0700

    Autocommit from 2026-05-11 10:13:00
    Klipper version: v0.13.0-610-g35ace5297
    
    Moonraker version: v0.10.0-19-g1ed102e
    
    Mainsail version: v2.17.0

--- autocommit.sh
+++ autocommit.sh
@@ -64,23 +64,8 @@ grab_version(){
   fi
 }
 
-# Here we dump stats database to text format for backup, IF the right software is found
-# To RESTORE the database, use the following command:
-# mdb_load -f ~/printer_data/config/data.mdb.backup -s -T ~/printer_data/database/
-
-if command -v /usr/bin/mdb_dump &> /dev/null
-then
-    if $history_only
-    then
-        echo "mdb_dump found! Exporting history table from data.mdb to ~/printer_data/config/data.mdb.backup"
-        mdb_dump -s history -n ~/printer_data/database/data.mdb -f ~/printer_data/config/data.mdb.backup
-    else
-        echo "mdb_dump found! Exporting ALL tables data.mdb to ~/printer_data/config/data.mdb.backup"
-        mdb_dump -a -n ~/printer_data/database/data.mdb -f ~/printer_data/config/data.mdb.backup
-    fi
-else
-    echo "mdb_dump not found! Consider installing it via 'sudo apt install lmdb-utils' if you want to back up your statistics database!"
-fi
+# Moonraker migrated from LMDB to SQLite (moonraker-sql.db) — mdb_dump no longer applies.
+# Database backup via mdb_dump disabled 2026-05-11.
 
 # To fully automate this and not have to deal with auth issues, generate a legacy token on Github
 # then update the command below to use the token. Run the command in your base directory and it will
